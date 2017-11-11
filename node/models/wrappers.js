572fe3a76ab81c97445d8c8086355f6b273e3bb0
ellismiranda <Ellis@MacBook-Pro.local>
2017-11-11 18:23:45 -0500

    [Add] new function for wrapping text to speech based on output from python

--- node/models/wrappers.js
+++ node/models/wrappers.js
@@ -13,3 +13,19 @@ function runPython(scriptName, scriptPath, onFunction) {
     console.log('finished');
   })
 }
+
+function textToSpeechWrapper() {
+  const pyshell = new PythonShell('main_script.py', { 'scriptPath': __dirname + '/../../'});
+
+  pyshell.on('message', (message) => {
+    if (message.startsWith('DEF_FLAG')) {
+      const definitions = parseDefinitions(message.substring(8));
+      speakManyText(definitions);
+    }
+  })
+
+  pyshell.end( (err) => {
+    if (err) throw err;
+    console.log('finished');
+  })
+}
