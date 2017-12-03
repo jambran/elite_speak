ab2ddccc6b6f8355366ec877acdafa97c45cb7b6
ellismiranda <Ellis@MacBook-Pro.local>
2017-12-03 13:02:12 -0500

    [add] commented out main method threading

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import os
 import main_script
 import defs
 import textToSpeech
+import threading
 
 app = Flask(__name__)
 
 
 @app.route('/record', methods=['POST'])
 def main():
+    # definitions = []
+    # thread = threading.Thread(target=main_script.main, args=[])
+    # thread.start()
     definitions = main_script.main()
     return render_template('list.html', definitions=definitions)
 
